99ce7f84ccf98da56caecf094dc7338db06a5e5d
Pruthviraj K <161309356+Pruthviraj394746@users.noreply.github.com>
2026-09-21 15:53:12 +0530

    Time: 12 ms (92.31%), Space: 93.6 MB (32.69%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
@@ -0,0 +1,35 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] ans = new long[k];
+
+        // dp[r] = number of subarrays ending at the
+        // previous index whose product % k == r
+        long[] dp = new long[k];
+
+        for (int num : nums) {
+            int value = num % k;
+
+            long[] next = new long[k];
+
+            // Start a new subarray with only this element
+            next[value]++;
+
+            // Extend all previous subarrays
+            for (int r = 0; r < k; r++) {
+                if (dp[r] > 0) {
+                    int newRemainder = (r * value) % k;
+                    next[newRemainder] += dp[r];
+                }
+            }
+
+            // Add all subarrays ending here to the answer
+            for (int r = 0; r < k; r++) {
+                ans[r] += next[r];
+            }
+
+            dp = next;
+        }
+
+        return ans;
+    }
+}
